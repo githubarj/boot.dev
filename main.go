5ca20737bb450be13b4c876e8398a5f7ed983f99
Richard Githuba <githuba9520@gmail.com>
2024-08-02 10:25:33 +0300

    declared the command struct

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	commands := [string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
 }
